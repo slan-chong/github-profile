20be282f1c534d1c9b46e0b5c61d4f478927a7f3
Slan <snchong.slan@gmail.com>
2022-05-31 01:28:41 +0800

    RWD

--- src/pages/GetName.jsx
+++ src/pages/GetName.jsx
@@ -5,17 +5,18 @@ const GetName = () => {
   const [input, setInput] = useState("");
   return (
     <div>
-      <div className="text-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="flex m-2">
+      <div className="text-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-2xl min-w-fit">
+        <div className="justify-self-center items-center m-2">
           <input
-            className="px-4 text-skin-dark border-2 border-gray-500 rounded-xl appearance-none w-full text-xl"
+            className="py-4 px-4 w-full text-skin-dark border-2 border-gray-500 rounded-xl appearance-none text-xl"
             onChange={(e) => setInput(e.target.value)}
             value={input}
             type="text"
             placeholder="Entry your Github username"
           />
+
           <div
-            className="px-4 py-3 m-2 border-2 text-xl cursor-pointer bg-skin-button-day hover:bg-skin-button-day-hover rounded-xl"
+            className="button m-2 px-4 py-4 rounded-full bg-gradient-to-r from-purple-500 to-pink-500 inline-block font-bold uppercase text-lg mt-4 text-center cursor-pointer w-36"
             onClick={() => {
               setUsername(input);
             }}
