9120d415b788c1099e2c218194dba5d0e36f13a9
Slan <snchong.slan@gmail.com>
2022-09-11 20:45:15 +0800

    submit a form using Enter key

--- src/pages/GetName.jsx
+++ src/pages/GetName.jsx
@@ -5,24 +5,25 @@ const GetName = () => {
   const [input, setInput] = useState("");
   return (
     <div>
-      <div className="m-2 text-center">
-        <input
-          className="py-4 px-4 text-skin-dark border-2 border-gray-500 rounded-xl appearance-none text-xl"
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
-          type="text"
-          placeholder="Entry your Github username"
-        />
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div className="m-2 text-center">
+          <input
+            className="py-4 px-4 text-skin-dark border-2 border-gray-500 rounded-xl appearance-none text-xl"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            type="text"
+            placeholder="Entry your Github username"
+          />
 
-        <div
-          className="button m-2 px-4 py-4 rounded-full bg-gradient-to-r from-purple-500 to-pink-500 inline-block font-bold uppercase text-lg mt-4 text-center cursor-pointer w-36"
-          onClick={() => {
-            setUsername(input);
-          }}
-        >
-          Submit
+          <button
+            className="button m-2 px-4 py-4 rounded-full bg-gradient-to-r from-purple-500 to-pink-500 inline-block font-bold uppercase text-lg mt-4 text-center cursor-pointer w-36"
+            onClick={() => setUsername(input)}
+            type="submit"
+          >
+            Submit
+          </button>
         </div>
-      </div>
+      </form>
       {username && <FetchUser username={username} />}
     </div>
   );
